44f48b7505232bd042140bc55b279820037671cc
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2024-04-25 12:17:02 +0200

    tests: update run-tests.sh
    * modernize it to make it look similar to, but a bit simpler than, the
      run-tests.sh in other invenio packages

--- run-tests.sh
+++ run-tests.sh
@@ -3,13 +3,20 @@
 #
 # This file is part of Invenio.
 # Copyright (C) 2016-2019 CERN.
+# Copyright (C) 2024 TU Wien.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-pydocstyle invenio_sipstore && \
-isort -rc -c -df **/*.py && \
-check-manifest --ignore ".travis-*" && \
-sphinx-build -qnNW docs docs/_build/html && \
-python setup.py test && \
-sphinx-build -qnNW -b doctest docs docs/_build/doctest
+# Quit on errors
+set -o errexit
+
+# Quit on unbound symbols
+set -o nounset
+
+export LC_TIME=en_US.UTF-8
+python -m check_manifest
+python -m sphinx.cmd.build -qnN docs docs/_build/html
+
+# Forward all arguments to this script to pytest
+python -m pytest "${@}"
