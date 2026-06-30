a8d6996e9e10d4ca0c0f2d57848432bbc2abc43b
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-06-30 13:40:56 +0200

    chore(setup): migrate from setuptools to hatchling
    * remove MANIFEST.in because that's only relevant for setuptools
    * format pyproject.toml with Tombi
    
    Co-authored-by: Sam Arbid <samaa2@kth.se>

--- run-tests.sh
+++ run-tests.sh
@@ -4,7 +4,6 @@
 
 pydocstyle invenio_sipstore && \
 isort -rc -c -df **/*.py && \
-check-manifest --ignore ".travis-*" && \
 sphinx-build -qnNW docs docs/_build/html && \
 python setup.py test && \
 sphinx-build -qnNW -b doctest docs docs/_build/doctest
